e2e3861c8cb27939454a61558a0e3e7f6b940d2d
Elle Vargas <vargas.elle@gmail.com>
2016-10-20 11:34:14 -0700

    created add, delete, size, and include methods

--- array-list.rb
+++ array-list.rb
@@ -1,25 +1,41 @@
 # Implementation of a list using a Native array --> ruby does this automatically
+# Do not use .anything except for .length
 
 class ArrayList
   def initialize
-    @storage = []
+    @storage = [0,0,0,0,0,0,0,0,0,0]
+    @size = 0
   end
 
   # Adds _value_ at the end of the list
   def add(value)
+    @storage[@size] = value
+    @size += 1
   end
 
   # Deletes the _last_ value in the array
   def delete
+    # @storage[@size] = 0
+    return @size -= 1
   end
 
   def display
+    toReturn = "["
+    @size.times do |i|
+      toReturn += @storage[i].to_s + ", "
+    end
+    print toReturn + "]"
   end
 
   def include?(key)
+    @size.times do |i|
+      return true if @storage[i] == key
+    end
+    return false
   end
 
   def size
+    return @size
   end
 
   def max
